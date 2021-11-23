fec2a7f2398c415ff26b311a12da0b3bd4e568cc
Lorenzo Fontoura <lorenzo@nioel.com>
2021-11-23 19:07:18 +1000

    update body

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("*", (request, response) => {
-  response.send("taxation is theft");
+  const body = "<!-- rellfy.com -->\n" +
+    "<p id=\"mises\"><b>taxation</b> is <b>theft</b><p>" +
+    "<script>document.querySelector(\"#mises\").onclick = " +
+    "() => window.location.href = \"https://mises.org\"</script>";
+  response.send(body);
 });
 
-app.serve(process.env.PORT || 80);
+app.listen(process.env.PORT || 80);
 
